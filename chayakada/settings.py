cf4ff08872803424018b46a4822e2be4f1eb260d
MrWings22 <albinthomas6210@gmail.com>
2025-11-21 01:25:31 +0530

    fixed:

--- chayakada/settings.py
+++ chayakada/settings.py
@@ -83,13 +83,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chayakada.wsgi.application'
 ASGI_APPLICATION = 'chayakada.asgi.application'
 
-REDIS_URL = config("REDIS_URL", default="redis://127.0.0.1:6379")
-
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [REDIS_URL],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
